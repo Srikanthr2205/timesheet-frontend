c0fe78da625b70f0c74d83a9615e7baad5a4742e
srikanthr2205 <srikanthr2205@gmail.com>
2025-08-08 22:36:26 +0530

    fixed hosting bug

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -2,6 +2,6 @@
 import axios from './axios';// assuming axios instance is configured here
 
 export const fetchAdminDashboardStats = async () => {
-  const response = await axios.get('/admin/dashboard');
+  const response = await axios.get('/api/admin/dashboard');
   return response.data;
 };
